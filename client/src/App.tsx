7685c4f77564d017aef3eab4d172a0ba0b1d8ba5
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-05 15:39:52 +0530

    add routing and create initial page components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Pricing from "./pages/Pricing";
+import Projects from "./pages/Projects";
+import MyProjects from "./pages/MyProjects";
+import Preview from "./pages/Preview";
+import Community from "./pages/Community";
+import View from "./pages/View";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<MyProjects />} />
 
-export default App
+        <Route path="/preview/:projectId" element={<Preview />} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/view/:projectId" element={<View />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
