b2764350f755dd371b43df25c739bcd40b9c2d0d
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-07 17:47:07 +0530

    add project display with preview and delete functionality to MyProjects page

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import type { Project } from "../types";
-import { Loader2Icon, PlusIcon } from "lucide-react";
+import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { dummyProjects } from "../assets/assets";
 
@@ -11,12 +11,14 @@ const MyProjects = () => {
   const navigate = useNavigate();
 
   const fetchProjects = async () => {
-    setProjects(dummyProjects)
+    setProjects(dummyProjects);
     // Simulate loading
     setTimeout(() => {
       setLoading(false);
     }, 1000);
   };
+
+  const deleteProject = async (projectId: string) => {};
 
   useEffect(() => {
     fetchProjects();
@@ -39,6 +41,69 @@ const MyProjects = () => {
                 <PlusIcon size={18} />
                 Create New
               </button>
+            </div>
+
+            <div className="flex flex-wrap gap-3.5">
+              {projects.map((project) => (
+                <div
+                  onClick={() => navigate(`/projects/${project.id}`)}
+                  key={project.id}
+                  className="relative group w-72 max-sm:mx-auto cursor-pointer bg-gray-900/60 border border-gray-700 rounded-lg overflow-hidden shadow-md group hover:shadow-indigo-700/30 hover:border-indigo-800/80 transition-all duration-300">
+                  {/* Desktop-like Mini Preview*/}
+                  <div className="relative w-full h-40 bg-gray-900 overflow-hidden border-b border-gray-800">
+                    {project.current_code ? (
+                      <iframe
+                        srcDoc={project.current_code}
+                        className="absolute top-0 left-0 w-[1200px] h-[800px] origin-top-left pointer-events-none"
+                        sandbox="allow-scripts allow-same-origin"
+                        style={{ transform: "scale(0.25)" }}
+                      />
+                    ) : (
+                      <div className="flex items-center justify-center h-full text-gray-500">
+                        <p>No Preview</p>
+                      </div>
+                    )}
+                  </div>
+                  {/* Content */}
+                  <div className="p-4 text-white bg-linear-180 from-transparent group-hover:from-indigo-950 to-transparent transition-colors">
+                    <div className="flex items-start justify-between">
+                      <h2 className="text-lg font-medium">{project.name}</h2>
+                      <button className="px-2.5 py-0.5 mt-1 ml-2 text-xs bg-gray-800 border border-gray-700 rounded-full">
+                        Website
+                      </button>
+                    </div>
+                    <p className="text-gray-400 mt-1 text-sm line-clamp-2">
+                      {project.initial_prompt}
+                    </p>
+
+                    <div
+                      onClick={(e) => e.stopPropagation()}
+                      className="flex justify-between  items-center mt-6">
+                      <span className="text-xs text-gray-500">
+                        {new Date(project.createdAt).toLocaleDateString()}
+                      </span>
+                      <div className="flex gap-3 text-white text-sm">
+                        <button
+                          onClick={() => navigate(`/preview/${project.id}`)}
+                          className="px-3 py-1.5 bg-white/10 hover:bg-white/15 rounded-md transition-all">
+                          Preview
+                        </button>
+                        <button
+                          onClick={() => navigate(`/projects/${project.id}`)}
+                          className="px-3 py-1.5 bg-white/10 rounded-md transition-all">
+                          Open
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                  <div onClick={(e) => e.stopPropagation()}>
+                    <TrashIcon
+                      className="absolute top-3 right-3 scale-0 group-hover:scale-100 bg-white p-1.5 size-7 rounded text-red-500 text-xl cursor-pointer transition-all"
+                      onClick={() => deleteProject(project.id)}
+                    />
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ) : (
