05ba4627e7e57c6380ba47c1d868bf9a2e4b9893
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-11 17:59:10 +0530

    feat(server): initialize server with Express, TypeScript, and CORS support

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import "dotenv/config";
+import cors from "cors";
+
+const app = express();
+
+const port = 3000;
+
+const corsOptions = {
+  origin: process.env.TRUSTED_ORIGINS?.split(",") || [],
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Server is Live!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
