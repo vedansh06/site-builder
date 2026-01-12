f7f432fbe3e802f8d2e3e0bb8cfb19e1d7876bee
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-12 16:00:12 +0530

    feat(auth): integrate better-auth for authentication with PostgreSQL support

--- server/server.ts
+++ server/server.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
@@ -11,6 +13,8 @@ const corsOptions = {
   credentials: true,
 };
 app.use(cors(corsOptions));
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is Live!");
