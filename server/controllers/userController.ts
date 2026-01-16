81ec18792ce4c06a46a3956965730ebb7c741bfd
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-16 13:22:11 +0530

    feat(user): add userId property to Request interface and enhance getUserCredits function

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -1,12 +1,21 @@
-import { Request, Response } from "express" 
+import { Request, Response } from "express";
+import prisma from "../lib/prisma.js";
 
 // Get User Credits
 export const getUserCredits = async (req: Request, res: Response) => {
-
-    try {
-        const userId = req.userId;
-    } catch (error) {
-        
+  try {
+    const userId = req.userId;
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
     }
 
-}
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    res.json({ credits: user?.credits });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
