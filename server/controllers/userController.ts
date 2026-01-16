d1cb307eaa0b878d17dbb37e47970d9ef1481a13
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-16 13:14:38 +0530

    feat(auth): implement user session protection middleware

---
+++ server/controllers/userController.ts
@@ -0,0 +1,12 @@
+import { Request, Response } from "express" 
+
+// Get User Credits
+export const getUserCredits = async (req: Request, res: Response) => {
+
+    try {
+        const userId = req.userId;
+    } catch (error) {
+        
+    }
+
+}
