6c6c17738d872969a9c36f96634c57ac934da5a5
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-16 13:30:02 +0530

    feat(project): implement createUserProject function with credit validation

--- server/controllers/userController.ts
+++ server/controllers/userController.ts
@@ -19,3 +19,42 @@ export const getUserCredits = async (req: Request, res: Response) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// Controller Function to create New Project
+export const createUserProject = async (req: Request, res: Response) => {
+  const userId = req.userId;
+  try {
+    const { initial_prompt } = req.body;
+
+    if (!userId) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+
+    if (user && user.credits < 5) {
+      return res
+        .status(403)
+        .json({ message: "add credits to create more projects" });
+    }
+
+    // Create a new project
+    const project = await prisma.websiteProject.create({
+      data: {
+        name:
+          initial_prompt.length > 50
+            ? initial_prompt.substring(0, 47) + "..."
+            : initial_prompt,
+        initial_prompt,
+        userId,
+      },
+    });
+
+    res.json({ credits: user?.credits });
+  } catch (error: any) {
+    console.log(error.code || error.message);
+    res.status(500).json({ message: error.message });
+  }
+};
