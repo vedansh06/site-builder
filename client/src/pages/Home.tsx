07e9e17461cf10b15a31adef9bd7b0294c942eec
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-18 12:38:38 +0530

    enhance project creation flow with user session validation and error handling

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,18 +1,38 @@
+import api from "@/configs/axios";
+import { authClient } from "@/lib/auth-client";
 import { Loader2Icon } from "lucide-react";
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "sonner";
 
 const Home = () => {
+  const { data: session } = authClient.useSession();
+
+  const navigate = useNavigate();
+
   const [input, setInput] = useState("");
   const [loading, setLoading] = useState(false);
 
   const onSubmitHandler = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    setLoading(true);
-    //* Simulate API call
-    setTimeout(() => {
+    try {
+      if (!session?.user) {
+        return toast.error("Please sign in to create a project");
+      } else if (!input.trim()) {
+        return toast.error("Please enter a message");
+      }
+      setLoading(true);
+      const { data } = await api.post("/api/user/project", {
+        initial_prompt: input,
+      });
       setLoading(false);
-    }, 3000);
+      navigate(`/projects/${data.projectId}`);
+    } catch (error: any) {
+      setLoading(false);
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
   };
 
   return (
