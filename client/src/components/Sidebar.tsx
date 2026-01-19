9751ef77ba9d5a9af186b10b6b82a8870e90f0f9
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-19 19:04:47 +0530

    feat(sidebar): implement project fetching and rollback functionality with error handling

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -8,6 +8,8 @@ import {
   UserIcon,
 } from "lucide-react";
 import { Link } from "react-router-dom";
+import api from "@/configs/axios";
+import { toast } from "sonner";
 
 interface SidebarProps {
   isMenuOpen: boolean;
@@ -27,14 +29,60 @@ const Sidebar = ({
   const messageRef = useRef<HTMLDivElement>(null);
   const [input, setInput] = useState("");
 
-  const handleRollback = async (versionId: string) => {};
+  const fetchProject = async () => {
+    try {
+      const { data } = await api.get(`/api/user/project/${project.id}`);
+      setProject(data.project);
+    } catch (error: any) {
+      toast.error(error.response?.data?.message || error.message);
+      console.log(error);
+    }
+  };
+
+  const handleRollback = async (versionId: string) => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure you want to rollback to this version? ",
+      );
+      if (!confirm) return;
+      setIsGenerating(true);
+      const { data } = await api.get(
+        `/api/project/rollback/${project.id}/${versionId}`,
+      );
+
+      const { data: data2 } = await api.get(`/api/user/project/${project.id}`);
+      toast.success(data.message);
+      setProject(data2.project);
+      setIsGenerating(false);
+    } catch (error: any) {
+      setIsGenerating(false);
+      toast.error(error.response?.data?.message || error.message);
+      console.log(error);
+    }
+  };
 
   const handleRevision = async (e: React.FormEvent) => {
     e.preventDefault();
-    setIsGenerating(true);
-    setTimeout(() => {
+    let interval: number | undefined;
+    try {
+      setIsGenerating(true);
+      interval = setInterval(() => {
+        fetchProject();
+      }, 10000);
+      const { data } = await api.post(`/api/project/revision/${project.id}`, {
+        message: input,
+      });
+      fetchProject();
+      toast.success(data.message);
+      setInput("");
+      clearInterval(interval);
       setIsGenerating(false);
-    }, 3000);
+    } catch (error: any) {
+      setIsGenerating(false);
+      toast.error(error.response?.data?.message || error.message);
+      console.log(error);
+      clearInterval(interval);
+    }
   };
 
   useEffect(() => {
@@ -55,7 +103,7 @@ const Sidebar = ({
             .sort(
               (a, b) =>
                 new Date(a.timestamp).getTime() -
-                new Date(b.timestamp).getTime()
+                new Date(b.timestamp).getTime(),
             )
             .map((message) => {
               const isMessage = "content" in message;
