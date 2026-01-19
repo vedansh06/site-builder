7d6fc3af54067c68fd4714d780f101e881109cbd
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-19 19:44:08 +0530

    implement project fetching and deletion features

--- client/src/pages/MyProjects.tsx
+++ client/src/pages/MyProjects.tsx
@@ -4,26 +4,53 @@ import { Loader2Icon, PlusIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
 import { dummyProjects } from "../assets/assets";
 import Footer from "../components/Footer";
+import api from "@/configs/axios";
+import { toast } from "sonner";
+import { authClient } from "@/lib/auth-client";
 
 const MyProjects = () => {
+  const { data: session, isPending } = authClient.useSession();
+
   const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState<Project[]>([]);
 
   const navigate = useNavigate();
 
   const fetchProjects = async () => {
-    setProjects(dummyProjects);
-    // Simulate loading
-    setTimeout(() => {
+    try {
+      const { data } = await api.get("/api/user/projects");
+      setProjects(data.projects);
       setLoading(false);
-    }, 1000);
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message);
+    }
   };
 
-  const deleteProject = async (projectId: string) => {};
+  const deleteProject = async (projectId: string) => {
+    try {
+      const confirm = window.confirm(
+        "Are you sure you want to delete this project?",
+      );
+      if (!confirm) return;
+
+      const { data } = await api.delete(`/api/project/${projectId}`);
+      toast.success(data.message);
+      fetchProjects();
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error?.response?.data?.message || error.message);
+    }
+  };
 
   useEffect(() => {
-    fetchProjects();
-  }, []);
+    if (session?.user && !isPending) {
+      fetchProjects();
+    } else if (!isPending && !session?.user) {
+      navigate("/");
+      toast("Please login to view your projects");
+    }
+  }, [session?.user]);
 
   return (
     <>
