ec6d9ed36ca8c1251e0dfb914132285dfda3addd
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-20 10:55:52 +0530

    purchase credits functionality with session handling

--- client/src/pages/Pricing.tsx
+++ client/src/pages/Pricing.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { appPlans } from "../assets/assets";
 import Footer from "../components/Footer";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
+import api from "@/configs/axios";
+import { any } from "better-auth";
 
 interface Plan {
   id: string;
@@ -12,9 +16,22 @@ interface Plan {
 }
 
 const Pricing = () => {
+  const { data: session } = authClient.useSession();
+
   const [plans] = React.useState<Plan[]>(appPlans);
 
-  const handlePurchase = async (planId: string) => {};
+  const handlePurchase = async (planId: string) => {
+    try {
+      if (!session?.user) return toast("Please login to purchase credits");
+
+      const { data } = await api.post("/api/user/purchase-credits", { planId });
+
+      window.location.href = data.payment_link;
+    } catch (error: any) {
+      toast.error(error?.response?.data?.message || error.message);
+      console.log(error);
+    }
+  };
 
   return (
     <>
