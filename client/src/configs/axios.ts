21b7d384344f7be74787a4e9cd5dd899cc71f7d0
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-22 19:15:24 +0530

    update axios baseURL to use production URL and add vercel configuration

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BASEURL || "http://localhost:3000",
+  baseURL:
+    import.meta.env.VITE_BASEURL || "https://site-builder-x0li.onrender.com",
   withCredentials: true,
 });
 
