8d1df3947f8759618daef70f4044f664bfcee965
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-22 10:43:28 +0530

    add vercel configuration and update build scripts in package.json

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "/",
+  build: {
+    outDir: "dist",
+  },
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
