9d645a4f8bf1b7c010323eb792a0713a2d5dd495
Vedansh Saini <vedanshsaini06@gmail.com>
2026-01-23 12:18:38 +0530

    fix VITE_BASEURL and axios configuration

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-    import.meta.env.VITE_BASEURL || "https://site-builder-x0li.onrender.com",
+    import.meta.env.VITE_BASEURL || "https://site-builder-2lw8.onrender.com",
   withCredentials: true,
 });
 
